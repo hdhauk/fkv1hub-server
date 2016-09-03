8e153b04a62bf782d553cb7b58675322c7afbbb4
halvor <halvor@mac-15.local>
2016-09-03 17:38:51 +0200

    Show runtime

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/kidoman/embd"
@@ -8,6 +9,7 @@ import (
 )
 
 func main() {
+	go printRunTime()
 	embd.InitGPIO()
 	defer embd.CloseGPIO()
 
@@ -15,8 +17,17 @@ func main() {
 	for {
 		embd.DigitalWrite(10, embd.High)
 		time.Sleep(1 * time.Second)
-		embd.DigitalWrite(10, emdb.Low)
+		embd.DigitalWrite(10, embd.Low)
 		time.Sleep(1 * time.Second)
 	}
 
+}
+
+func printRunTime() {
+	start := time.Now()
+	fmt.Println("FKV1HUB-Server\n------------------")
+	for {
+		fmt.Printf("Server runtime: %.F seconds\r", time.Since(start).Seconds())
+		time.Sleep(1 * time.Second)
+	}
 }
