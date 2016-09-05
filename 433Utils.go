147da14482dfd0080d89ccc75ac3a14a2e6164bc
Halvor <hh@surface.com>
2016-09-05 11:03:09 +0200

    Splits up the code into utils and main. Prepare 433utils to be separated out as a package

---
+++ 433Utils.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// For using labels on raspberry-pi breakout board
+const (
+	SDA    = 8
+	SCL    = 9
+	GPIO04 = 7
+	GPIO17 = 0
+	GPIO27 = 2
+	GPIO22 = 3
+	MOSI   = 12
+	MISO   = 13
+	SCLK   = 14
+
+	TxD    = 15
+	RxD    = 16
+	GPIO18 = 1
+	GPIO23 = 4
+	GPIO24 = 5
+	GPIO25 = 6
+	CE0    = 10
+	CE1    = 11
+)
+
+// RxCodeHandler is a function that run concurrently when a 433Mhz
+// 8-bit code is recieved. It takes the code as an input
+type RxCodeHandler func(int8)
+
+func listenAndRespond(pin int, handler RxCodeHandler) {
+	for {
+		code := listen(pin)
+		go handler(code)
+		time.Sleep(500 * time.Millisecond) // Make sure signal has died out...
+	}
+}
+
+func listen(pin int) int {
+	// For correct pinout: https://projects.drogon.net/raspberry-pi/wiringpi/pins/
+	// or use constants declared above
+	pinStr := strconv.Itoa(pin)
+	// TODO: Configure this to go in a config file
+	// TODO: Test that choosing a diffrent pin works
+	out, err := exec.Command("/home/pi/Documents/C++/433Utils/RPi_utils/SingleSniffer", pinStr).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	code, err := strconv.Atoi(strings.TrimSpace(string(out)))
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	return code
+}
--- main.go
+++ main.go
@@ -2,20 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
-	"strconv"
 	"time"
-	"strings"
 
-	"github.com/kidoman/embd"
 	_ "github.com/kidoman/embd/host/rpi"
 )
 
 func main() {
 	go printRunTime()
 	//go blinkLED()
-	listenAndRespond(1, dummyHandler)
+	listenAndRespond(GPIO18, dummyHandler)
 
 }
 
@@ -28,47 +23,6 @@ func printRunTime() {
 	}
 }
 
-func blinkLED() {
-	embd.InitGPIO()
-	defer embd.CloseGPIO()
-
-	pin, err := embd.NewDigitalPin(10)
-	if err != nil {
-		fmt.Println("Something went wrong when assigning new pin")
-		fmt.Println(err)
-	}
-	pin.SetDirection(embd.Out)
-	for {
-		pin.Write(embd.High)
-		time.Sleep(2 * time.Second)
-		pin.Write(embd.Low)
-		time.Sleep(2 * time.Second)
-	}
-}
-
-func listenAndRespond(pin int, handler func(int)) {
-	for {
-		code := listen(pin)
-		go handler(code)
-		time.Sleep(500 * time.Millisecond) // Make sure signal has died out...
-	}
-}
-
-func listen(pin int) int {
-	// For correct pinout: https://projects.drogon.net/raspberry-pi/wiringpi/pins/
-	pinStr := strconv.Itoa(pin)
-	out, err := exec.Command("/home/pi/Documents/C++/433Utils/RPi_utils/SingleSniffer", pinStr).Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	code, err := strconv.Atoi(strings.TrimSpace(string(out)))
-	if err != nil {
-		fmt.Println(err)
-		return -1
-	}
-	return code
-}
-
-func dummyHandler(code int) {
+func dummyHandler(code int8) {
 	fmt.Printf("Recived code: %d\n", code)
 }
