fd5da1976bbc460012d0ebb536308e1e5c951d67
Halvor <hh@surface.com>
2016-09-06 09:49:06 +0200

    Added more tests

--- 433Utils_test.go
+++ 433Utils_test.go
@@ -9,6 +9,11 @@ func TestEncode(t *testing.T) {
 	}{
 		{Message{11, 22, 3333}, 11223333},
 		{Message{22, 33, 4444}, 22334444},
+		{Message{10, GetTemp, 0000}, 10100000},
+		{Message{11, SetTemp, 2222}, 11112222},
+		{Message{22, GetHumid, 0000}, 22200000},
+		{Message{1, 1, 0}, 1010000},
+		{Message{0, 0, 0}, 0},
 	}
 
 	for _, test := range tests {
